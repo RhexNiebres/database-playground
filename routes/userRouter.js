a16ee66cf353d8a56cb9683fcac2344ee1256fa4
RhexNiebres <rhexniebres@gmail.com>
2025-03-01 18:42:34 -0800

    feat: add search logic with filter

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,9 +2,10 @@ const { Router } = require("express");
 const usersController = require("../controllers/usersController");
 const usersRouter = Router();
 
-usersRouter.get("/", usersController.usersListGet);
-console.log('im here')
-usersRouter.get("/new", usersController.usersCreateGet);
-usersRouter.post("/new", usersController.usersCreatePost);
+usersRouter.get("/", usersController.getUsernames);
+console.log('im here in routes')
+usersRouter.get("/new", usersController.createUsernameGet);
+usersRouter.post("/new", usersController.createUsernamePost);
+usersRouter.get("/search", usersController.searchUsernames)
 
 module.exports = usersRouter;
