491ed969eae182cce37f64f93fcd959b664340e0
RhexNiebres <rhexniebres@gmail.com>
2025-03-01 16:21:07 -0800

    add logic for controller functions

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,18 +1,26 @@
+const db = require("../db/queries");
 
+async function getUsernames(req, res) {
+  const usernames = await db.getAllUsernames();
+  console.log("Usernames: ", usernames);
+  res.send("Usernames: " + usernames.map(user => user.username).join(", "));
+}
 
-exports.usersListGet = (req, res) => {
-  console.log("usernames will be logged here - wip");
-  res.render("index", {title: 'users'});
-};
-
-exports.usersCreateGet = (req, res) => {
+async function createUsernameGet(req, res) {
   res.render("createUser", {
     title: "Create user",
   });
-};
+}
 
-exports.usersCreatePost = (req, res) => {
-  console.log("username to be saved: ", req.body.username);
+async function createUsernamePost(req, res) {
+  const { username } = req.body;
+  await db.insertUsername(username);
   res.redirect("/");
+}
+
+module.exports = {
+  getUsernames,
+  createUsernameGet,
+  createUsernamePost
 };
 
